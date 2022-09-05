86978c3ee3906781e20d4c15e2ce8811f8a1ef9d
Mickey <mickeyc11111@gmail.com>
2022-09-05 09:54:17 -0400

    commit tree a5551985d34ea7bc1c316cee20a83746985227d3

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator arr
+    arr.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms arr
+    arr.map.with_index(1) do |name, index|
+        "Hello, #{name}! You'll be assigned to room #{index}!"
+    end
+end
+
+def printer arr
+    batch_badge_creator(arr).each do |greeting|
+        puts greeting
+    end
+    assign_rooms(arr).each do |room|
+        puts room
+    end
+end
